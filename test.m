fdf183f4056e6e06475c45bef2d00126ba7ffd07
haichenl <haichenl@andrew.cmu.edu>
2014-12-23 15:44:59 -0500

    test script fix

--- test.m
+++ test.m
@@ -2,5 +2,5 @@
 
 str1 = ['O',char(10),'H 1 R', char(10),'H 1 R 2 A',char(10),char(10),'R = .9', char(10),'A = 104.5'];
 str2 = '6-31g*';
-matpsi = MatPsi(str1,str2);
+matpsi = MatPsi2(str1,str2);
 
