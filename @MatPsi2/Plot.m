14bda5e19d3558a97c618a2c4eb11fded1a6a92d
haichenl <haichenl@andrew.cmu.edu>
2014-12-25 03:37:05 -0500

    plot improve

--- @MatPsi2/Plot.m
+++ @MatPsi2/Plot.m
@@ -1,65 +1,58 @@
 function Plot(obj)
-cartesian = obj.Molecule_Geometry() .* 0.529177249;
+cartesian = [reshape(obj.Molecule_AtomicNumbers(), [], 1), ...
+    obj.Molecule_Geometry() .* 0.529177249];
 
+distMat = dist(cartesian(:, 2:end)');
 numAtoms = size(cartesian, 1);
-maxNumBondsVec = zeros(numAtoms, 1);
-for iAtom = 1:numAtoms
-    maxNumBondsVec(iAtom) = MaxNumBonds(cartesian(iAtom, 1));
-end
 
-[maxNumBondsVec, order] = sort(maxNumBondsVec, 'descend');
-sorted_cartesian = cartesian(order,:);
+maxBonds = zeros(numAtoms, 1);
+for iAtom = 1:numAtoms
+    maxBonds(iAtom) = MaxNumBonds(cartesian(iAtom, 1));
+end
 
 numBondsMat = zeros(numAtoms);
-for iAtom = 1:numAtoms
-    for jAtom = iAtom+1:numAtoms
-        numBondsMat(iAtom, jAtom) = NumberOfBonds(sorted_cartesian, iAtom, jAtom);
-    end
-end
-numBondsMat = numBondsMat + numBondsMat';
-
-for iAtom = 1:numAtoms
-    [temp, tempOrder] = sort(numBondsMat(iAtom, :));
-    cumTemp = cumsum(temp);
-    for i = 1:length(cumTemp)
-        if(cumTemp(i) > maxNumBondsVec(iAtom))
-            numBondsMat(iAtom, tempOrder(i)) = maxNumBondsVec(iAtom) - cumTemp(i-1);
-            numBondsMat(tempOrder(i), iAtom) = numBondsMat(iAtom, tempOrder(i));
-            numBondsMat(iAtom, tempOrder(i+1:end)) = 0;
-            numBondsMat(tempOrder(i+1:end), iAtom) = 0;
-            break;
+for iter = [1 100]
+    for iAtom = 1:numAtoms
+        [~, tempOrder] = sort(distMat(iAtom, :));
+        for jAtom = tempOrder(2:end)
+            if(sum(numBondsMat(iAtom, :)) < maxBonds(iAtom) ...
+                    && sum(numBondsMat(jAtom,:)) < maxBonds(jAtom))
+                maxBetween = min(maxBonds(iAtom), maxBonds(jAtom));
+                numBonds = min(min(NumBonds(cartesian, iAtom, jAtom), maxBetween), iter);
+                numBondsMat(iAtom, jAtom) = numBonds;
+                numBondsMat(jAtom, iAtom) = numBonds;
+            end
         end
     end
 end
 
 bondLines = {};
 for iAtom = 1:numAtoms
-    iPos = sorted_cartesian(iAtom, 2:end);
+    iPos = cartesian(iAtom, 2:end);
     for jAtom = iAtom+1:numAtoms
-        jPos = sorted_cartesian(jAtom, 2:end);
+        jPos = cartesian(jAtom, 2:end);
         numBonds = numBondsMat(iAtom, jAtom);
         vector = iPos - jPos;
-        normal_vector = ones(1, 3);
-        normal_vector(3) = -(vector(1) + vector(2))/vector(3);
-        normal_vector = normal_vector ./ norm(normal_vector);
+        normVec = ones(1, 3);
+        normVec(3) = -(vector(1) + vector(2))/vector(3);
+        normVec = normVec ./ norm(normVec);
+        ijVecs = [iPos; jPos];
+        normVecs = 0.05 .* [normVec; normVec];
         switch(numBonds)
             case(1) % single bond
-                bondLines{end+1} = [iPos; jPos];
+                bondLines{end+1} = ijVecs;
             case(2) % double bond
-                bondLines{end+1} = [iPos; jPos] ...
-                    - 0.03.*[normal_vector; normal_vector];
-                bondLines{end+1} = [iPos; jPos] ...
-                    + 0.03.*[normal_vector; normal_vector];
+                bondLines{end+1} = ijVecs - normVecs;
+                bondLines{end+1} = ijVecs + normVecs;
             case(3)% triple bond
-                bondLines{end+1} = [iPos; jPos];
-                bondLines{end+1} = [iPos; jPos] ...
-                    - 0.05.*[normal_vector; normal_vector];
-                bondLines{end+1} = [iPos; jPos] ...
-                    + 0.05.*[normal_vector; normal_vector];
+                bondLines{end+1} = ijVecs;
+                bondLines{end+1} = ijVecs - normVecs;
+                bondLines{end+1} = ijVecs + normVecs;
         end
     end
 end
 
+figure();
 set(gcf,'color','w');
 axis vis3d;
 axis off;
@@ -79,36 +72,49 @@ allColors = 0.12 .* [ ...
     [0 6 6]; [6 0 6]; [6 6 0]; [3 3 3]; [0 0 6]; [6 0 0]; [0 6 0]; [4 3 3];
     [0 4 4]; [4 0 4];];
 
-shapes = allShapes(sorted_cartesian(:, 1));
-colors = allColors(sorted_cartesian(:, 1), :);
-scatter3(sorted_cartesian(:, 2), sorted_cartesian(:, 3), sorted_cartesian(:, 4), shapes, colors, 'fill');
+scatter3(cartesian(:, 2), cartesian(:, 3), cartesian(:, 4), ...
+    allShapes(cartesian(:, 1)), allColors(cartesian(:, 1), :), 'fill');
 
 end
 
 
-function numBonds = NumberOfBonds(cartesian, indAtom1, indAtom2)
+function maxNumBonds = MaxNumBonds(atomicNum)
+[~, col, rowMax] = FromAtomicNumber(atomicNum);
+maxNumBonds = min(col, rowMax-col);
+end
+
+function numBonds = NumBonds(cartesian, indAtom1, indAtom2)
 
 distance = norm(cartesian(indAtom1, 2:end) - cartesian(indAtom2, 2:end));
 z1 = cartesian(indAtom1, 1);
 z2 = cartesian(indAtom2, 1);
 
-coeffs = [-2.6306 1.1553 0.4061 -0.0065 0.0836 -0.0168 -0.0339, ...
-    0.0010 -0.2922 0.4120 -0.2016 0.3171 0.0173 0.0019 2.5450]';
+coeffs = [ ...
+    -2.630575471115972
+    1.155281331403750
+    0.406101379591945
+    -0.006491787214362
+    0.083551824390146
+    -0.016819474606420
+    -0.033933135725531
+    0.000967977472307
+    -0.292248868653409
+    0.411978769668091
+    -0.201582865554906
+    0.317088796767966
+    0.017315986848049
+    0.001901847913666
+    2.544990894929442];
 
 criterions(1) = Feature(z1, z2) * coeffs;
-criterions(2) = criterions(1) ./ 1.1 .* ...
+criterions(2) = criterions(1) ./ 1.05 .* ...
     ~~max(min(MaxNumBonds(z1), MaxNumBonds(z2))-1, 0);
-criterions(3) = criterions(1) ./ 1.2 .* ...
+criterions(3) = criterions(1) ./ 1.15 .* ...
     ~~max(min(MaxNumBonds(z1), MaxNumBonds(z2))-2, 0);
-criterions(1) = criterions(1).*1.2;
+criterions(1) = 1.2 .* criterions(1);
 
 numBonds = 3 - sum(distance>criterions);
 
-end
-
-function maxNumBonds = MaxNumBonds(atomicNum)
-[~, col, rowMax] = FromAtomicNumber(atomicNum);
-maxNumBonds = min(col, rowMax-col);
 end
 
 function feat = Feature(z1, z2)
